0b765b5230bb28492bf954d6a724a4de2f7de0f5
Kirill Volkovich <volkovich@scand.com>
2017-01-30 11:41:14 +0300

    Implemented 'ElementRectWatcher' component
    Component to easy handle element size and position changes.
    It can be usefull to make something like media queries but based on element container size instead of not on screen size.

---
+++ src/client/components/ElementRectWatcher/ElementRectWatcher.react.js
@@ -0,0 +1,48 @@
+import React, { PropTypes, Component } from 'react';
+
+export default
+class ElementRectWatcher extends Component {
+  componentDidMount() {
+    this.calculateSize();
+  }
+
+  componentWillUnmount() {
+    this.cancelCalculateSize();
+  }
+
+  calculateSize(node) {
+    let rect = this._node.getBoundingClientRect() || {};
+    this.props.onChange(rect);
+    this._animationFrame = window.requestAnimationFrame(() => this.calculateSize(node));
+  }
+
+  cancelCalculateSize() {
+    if (this._animationFrame) {
+      cancelAnimationFrame(this._animationFrame);
+    }
+  }
+
+  render() {
+    let {
+      children,
+      onChange,
+      ...restProps
+    } = this.props;
+
+    return (
+      <div
+        {...restProps}
+        ref={ref => (this._node = ref)}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+ElementRectWatcher.propTypes = {
+  onChange: PropTypes.func
+};
+ElementRectWatcher.defaultProps = {
+  onChange: () => {}
+};
