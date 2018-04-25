ba6437473277fda835bc6cb4b6f5768986d5362d
Dmitry Shienok <dshienok@scand.com>
2018-04-25 13:28:07 +0300

    Created new release branch. [ci skip]

--- src/client/index.js
+++
@@ -1,8 +0,0 @@
-// Here should be es2016 or CommonJS exports.
-
-module.exports = {
-  ElementRectWatcher: require('./components/ElementRectWatcher').default,
-  SimpleModal: require('./components/SimpleModal').default,
-  StickyNode: require('./components/StickyNode').default,
-  TooltipBase: require('./components/TooltipBase').default
-};
